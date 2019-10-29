6ba86d6b9dbdbda10eb7bfbba4dc93b74a936d2e
J Ross <j.ross@centre.edu>
2019-10-28 22:45:19 -0400

    shell

--- musicpage.js
+++ musicpage.js
@@ -1,1 +1,17 @@
+//2c7gFThUYyo2t6ogAglYNw
+//2FXC3k01G6Gw61bmprjgqS
 
+var musicpromise = d3.json("musicpage.json");
+
+musicpromise.then (
+    
+    function(data)
+    {
+    console.log("works",data);
+    }
+    function(err)
+    {
+        console.log("nope", err)
+    }
+                      
+)
